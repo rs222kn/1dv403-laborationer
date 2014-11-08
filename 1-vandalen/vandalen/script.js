860b1206ad1c1d94eae68c160c279757220980c1
Richard Söderman <rs222kn@student.lnu.se>
2014-11-08 13:22:25 +0000

    saker som ar fel uppgift 1 d

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,8 +4,55 @@ var makePerson = function(persArr){
 
 
 	// Din kod här...
-	
-	
-
+	//console.log(persArr); 
+    /*
+    var nameArray = [];
+    var nameString = "";
+                                                    ALLT = FEEEEL
+   var minValueArr = [];
+   var ValueArr = [];
+   
+   var sum = 0;
+   
+   function loop(){
+       for (var i = 0; i < persArr.length; i++) {
+            ValueArr[i] = persArr[i].age;
+            sum += ValueArr[i]; 
+        }
+   }
+   console.log("hej " + loop());
+   
+   var obj = {
+        minAge: function(){
+             
+            return ValueArr.min();
+        },
+        maxAge: function(){
+            
+            return ValueArr.max();
+        },
+        averageAge: function(){
+            return sum/ValueArr.length;
+        },
+        names: function(){
+            //persArr.sort();
+            for (var i = 0; i < persArr.length; i++) {
+                
+                nameArray[i] = persArr[i].name;
+            }
+            nameArray.sort();
+            nameString  = nameArray.join(", ");
+            console.log(g);
+            return nameString;
+           
+        } 
+   };
+    //console.log(obj.names());
+    return obj;
+    */
 }
+
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var result = makePerson(data);
+console.log(result);
 
