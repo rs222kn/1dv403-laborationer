1dad08bf976851ae3ac1d3dd06a1caba74698c86
Richard Söderman <rs222kn@student.lnu.se>
2014-12-03 20:53:56 +0000

    it basically works

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -31,9 +31,9 @@ var Memory = {
                     cell: document.createElement("td"),
                     a: document.createElement("a"),
                     img: document.createElement("img")
-                }
+                };
                 
-                var cells = (i * cellX) + j; // antalet celler i gridd
+               // var cells = (i * cellX) + j; // antalet celler i gridd
                 
                 // attributer
                 create.a.setAttribute("href", "#");
@@ -58,37 +58,73 @@ var Memory = {
     cellPress: function(gridSelector){
         
         for (var i = 0; i < gridSelector.length; i++) {
-                
             call(i);
         }
+        
         function call(j) {
-            
             gridSelector[j].addEventListener("click",function(e){
                press(e, j, Memory.ranImg[0][j]);
            });
         }
         
+        // Truck på en av alla cell'er
         function press(e, it, ranImg){
-            Memory.timers(0, it, ranImg);
-            Memory.imgTagIndex[it].img.setAttribute("src", 'pics/'+ranImg+'.png'); // byter bild när man klickar på den specifik cell
             
-        }
-    },
-    timers: function(timerId, it){
-        var timeoutID;
-        
-        var time = {
-            changeBackPic: function(){
-                timeoutID = window.setTimeout(function(){
-                    Memory.imgTagIndex[it].img.setAttribute("src", 'pics/0.png');
-                }, 1000);
+            if(Memory.timesPressed == 0){ // cell tryck 1
+                Memory.imgTagIndex[it].img.setAttribute("src", 'pics/'+ranImg+'.png'); // byter bild när man klickar på den specifik cell
+                
+                Memory.itChe1.id = it; // sparar nuvarande värden så dom kan användas senare
+                Memory.itChe1.imgNr = ranImg;
+                
+                Memory.timesPressed++;
+            }else if(Memory.timesPressed == 1){ // cell tryck 2
+                Memory.imgTagIndex[it].img.setAttribute("src", 'pics/'+ranImg+'.png'); // byter bild när man klickar på den specifik cell
+                
+                Memory.itChe2.id = it; // sparar nuvarande värden så dom kan användas senare
+                Memory.itChe2.imgNr = ranImg;
+                
+                // kollar om dom båda har samma imgNr (altså dom har samma bild)
+                if(Memory.itChe1.imgNr === Memory.itChe2.imgNr){
+                    console.log("lika");
+                }else{ // om inte kallar vi på timer som tar bort bilderna
+                    console.log("inte lika");
+                    Memory.timers(0 , Memory.itChe1.id, Memory.itChe2.id);
+                    
+                }
+            
+                // nollställer allt (Memory.itChe'X' behövs inte nollställas egntligen..)
+                Memory.timesPressed = 0;
+                Memory.itChe1.id = null;
+                Memory.itChe1.imgNr = null;
+                Memory.itChe2.id = null;
+                Memory.itChe2.imgNr = null;
             }
         }
+    },
+    
+    itChe1: {id: null, imgNr: null}, // objekt som lagrar värdena på det gammla cell'trycket en för vardera cell 
+    itChe2: {id: null, imgNr: null},
+    timesPressed: 0,
+    
+    timeoutID: null, // timeout id för timer om man skulle vilja avbryta den.
+    
+    timers: function(timerId, it1, it2){
+        var time = {
+            changeBackPic: function(){
+                Memory.timeoutID = window.setTimeout(function(){
+                    // byter till standard bild efter 1sec
+                    Memory.imgTagIndex[it1].img.setAttribute("src", 'pics/0.png'); 
+                    Memory.imgTagIndex[it2].img.setAttribute("src", 'pics/0.png');
+                }, 1000);
+            }
+        };
         
         switch(timerId){
             case 0:
                 time.changeBackPic();
+                break;
             case 1:
+                clearTimeout(Memory.timeoutID);
                 break;
             case 2:
                 break;
